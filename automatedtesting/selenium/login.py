5771a68742a1e97ba620a66d791407dd4af00a7b
SudKul <sudhanshu.kulshrestha@gmail.com>
2022-09-23 18:04:09 +0530

    Update testscripts

--- automatedtesting/selenium/login.py
+++ automatedtesting/selenium/login.py
@@ -1,4 +1,4 @@
-# #!/usr/bin/env python
+#!/usr/bin/env python
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 
@@ -6,13 +6,15 @@ from selenium.webdriver.chrome.options import Options as ChromeOptions
 # Start the browser and login with standard_user
 def login (user, password):
     print ('Starting the browser...')
-    # --uncomment when running in Azure DevOps.
-    # options = ChromeOptions()
-    # options.add_argument("--headless") 
-    # driver = webdriver.Chrome(options=options)
-    driver = webdriver.Chrome()
+    options = ChromeOptions()
+    options.add_argument("--headless")
+    driver = webdriver.Chrome(options=options)
     print ('Browser started successfully. Navigating to the demo page to login.')
     driver.get('https://www.saucedemo.com/')
-
-login('standard_user', 'secret_sauce')
-
+    print ('Logging in with user: ' + user)
+    driver.find_element_by_id('user-name').send_keys(user)
+    driver.find_element_by_id('password').send_keys(password)
+    driver.find_element_by_css_selector('.btn_action').click()
+    assert "/inventory.html" in driver.current_url
+    print ('Logged in successfully!')
+    return driver
